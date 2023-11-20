6d77db86668cec2c4c8be95f21a16b2b42c575f9
Hamza Sušić <hamzasusic@Hamzas-MacBook-Air.local>
2023-11-20 13:26:46 +0100

    Forgotten save

--- front-end/shoppingListScript.js
+++ front-end/shoppingListScript.js
@@ -1,3 +1,4 @@
+
 // Define the server URL variable
 const serverURL = 'http://127.0.0.1:4999';
 
@@ -19,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     const row = shoppingListTable.insertRow();
                     const cell1 = row.insertCell(0);
                     const cell2 = row.insertCell(1);
-                    const cell3 = row.insertCell(3)
+                    const cell3 = row.insertCell(2);
 
                     cell1.textContent = item.item_id;
                     cell2.textContent = item.name;
@@ -29,5 +30,21 @@ document.addEventListener("DOMContentLoaded", function () {
             .catch(error => {
                 console.error('Error:', error);
             });
+    });
+
+    // Add event listener to the total price button
+    document.getElementById('display-total-btn').addEventListener('click', function () {
+        const totalAmountElement = document.getElementById('total-amount');
+        const shoppingListTable = document.getElementById('shopping-list-table');
+        
+        let totalPrice = 0;
+
+        // Iterate through rows and sum up item prices
+        for (let i = 1; i < shoppingListTable.rows.length; i++) {
+            totalPrice += parseFloat(shoppingListTable.rows[i].cells[2].textContent);
+        }
+
+        // Display the total price
+        totalAmountElement.textContent = totalPrice.toFixed(2); 
     });
 });
